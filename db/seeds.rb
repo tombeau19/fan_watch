2e67bc2bb1129c3bd1b1a9a2258754ed1b920c32
tombeau19 <thomas.beauregard@gmail.com>
2017-11-10 15:44:42 -0500

    added react app in clien folder, put in procfile and packagejson

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,148 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Team.destroy_all
+
+# AFC EAST
+Team.create({
+    name: 'New England Patriots'
+    logo:
+})
+Team.create({
+    name: 'New York Jets'
+    logo:
+})
+Team.create({
+    name: 'Buffalo Bills'
+    logo:
+})
+Team.create({
+    name: 'Miami Dolphins'
+    logo:
+})
+
+# AFC WEST
+Team.create({
+    name: 'Denver Broncos'
+    logo:
+})
+Team.create({
+    name: 'Kansas City Chiefs'
+    logo:
+})
+Team.create({
+    name: 'Los Angeles Chargers'
+    logo:
+})
+Team.create({
+    name: 'Oakland Raiders'
+    logo:
+})
+
+# AFC NORTH
+Team.create({
+    name: 'Pittsburgh Steelers'
+    logo:
+})
+Team.create({
+    name: 'Cincinnati Bengals'
+    logo:
+})
+Team.create({
+    name: 'Baltimore Ravens'
+    logo:
+})
+Team.create({
+    name: 'Cleveland Browns'
+    logo:
+})
+
+# AFC SOUTH
+Team.create({
+    name: 'Tennessee Titans'
+    logo:
+})
+Team.create({
+    name: 'Jacksonville Jaguars'
+    logo:
+})
+Team.create({
+    name: 'Houston Texans'
+    logo:
+})
+Team.create({
+    name: 'Indianapolis Colts'
+    logo:
+})
+
+# NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC NFC
+
+# NFC EAST
+Team.create({
+    name: 'Philadelphia Eagles'
+    logo:
+})
+Team.create({
+    name: 'Dallas Cowboys'
+    logo:
+})
+Team.create({
+    name: 'Washington Redskins'
+    logo:
+})
+Team.create({
+    name: 'New York Giants'
+    logo:
+})
+
+# NFC WEST
+Team.create({
+    name: 'Los Angeles Rams'
+    logo:
+})
+Team.create({
+    name: 'Seattle Seahawks'
+    logo:
+})
+Team.create({
+    name: 'Arizona Cardinals'
+    logo:
+})
+Team.create({
+    name: 'San Francisco 49ers'
+    logo:
+})
+
+# NFC NORTH
+Team.create({
+    name: 'Minnesota Vikings'
+    logo:
+})
+Team.create({
+    name: 'Detroit Lions'
+    logo:
+})
+Team.create({
+    name: 'Green Bay Packers'
+    logo:
+})
+Team.create({
+    name: 'Chicago Bears'
+    logo:
+})
+
+# NFC SOUTH
+Team.create({
+    name: 'New Orleans Saints'
+    logo:
+})
+Team.create({
+    name: 'Carolina Panthers'
+    logo:
+})
+Team.create({
+    name: 'Atlanta Falcons'
+    logo:
+})
+Team.create({
+    name: 'Tampa Bay Buccaneers'
+    logo:
+})
+
