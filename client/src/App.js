dcc693fd07a179e09845b2a7aa1f5ebb964ef4ba
tombeau19 <thomas.beauregard@gmail.com>
2017-11-17 09:49:35 -0500

    added landing page that goes away after first click

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,42 @@ import TeamList from './components//Teams/TeamList'
 import TeamPage from './components/Teams/TeamPage'
 import NavBar from './components/NavBar/NavBar'
 import TeamNavBar from './components/NavBar/TeamNavBar'
+import styled from 'styled-components'
+
+const Landing = styled.div`
+background: url('http://www.kenn.com/images/blog/nfl_ref002.jpg') no-repeat center center fixed; 
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+padding: 0;
+margin: 0;
+height: 100vh;
+width: 100vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const Arrow = styled.img`
+width: 75px;
+height: 75px;
+`
+
+const Title = styled.h1`
+color: #EA8538;
+font-family: 'Coda Caption', sans-serif;
+font-size: 100px;
+margin: 0px
+`
+
+const Intro = styled.h1`
+color: #EA8538;
+font-family: 'Coda Caption', sans-serif;
+font-size: 30px;
+padding-bottom: 50px;
+`
 
 class App extends Component {
 
@@ -13,7 +49,8 @@ class App extends Component {
     teams: [],
     team: {},
     redirectToTeamPage: false,
-    homePage: true
+    homePage: true,
+    hideLanding: true
   }
 
   async componentWillMount() {
@@ -46,10 +83,14 @@ class App extends Component {
   }
 
   setHomePageNavBar = async () => {
-    await this.setState({ 
+    await this.setState({
       redirectToTeamPage: !this.state.setRedirectToTeamPage,
       homePage: !this.state.homePage,
-     })
+    })
+  }
+
+  setHideLanding = async (event) => {
+    await this.setState({ hideLanding: !this.state.hideLanding })
   }
 
   setRedirectToTeamPage = async () => {
@@ -62,12 +103,22 @@ class App extends Component {
     return (
       <Router>
         <div>
-          {this.state.homePage ? <NavBar /> : <TeamNavBar setHomePageNavBar={this.setHomePageNavBar} team={this.state.team} />}
-          {this.state.redirectToTeamPage ? <Redirect to={`/${this.state.team.nick_name}`} /> : <Redirect to={`/`} />}
-          <Switch>
-            <Route exact path='/' render={TeamListComponent} />
-            <Route path='/:team_id' render={TeamPageComponent} />
-          </Switch>
+          {this.state.hideLanding ? <Landing>
+            <Title>Fan Watch</Title>
+            <Intro>No more arguments on where to watch YOUR team</Intro>
+            <a onClick={this.setHideLanding} href="#secondHalf">
+              <Arrow src='https://alexkessock.com/img/icon_53743.png' alt='arrow icon' />
+            </a>
+          </Landing> : null}
+          <div>
+            {this.state.homePage ? <NavBar /> : <TeamNavBar setHomePageNavBar={this.setHomePageNavBar} team={this.state.team} />}
+            {this.state.redirectToTeamPage ? <Redirect to={`/${this.state.team.nick_name}`} /> : <Redirect to={`/`} />}
+            <Switch>
+              <Route exact path='/' render={TeamListComponent} />
+              <Route path='/:team_id' render={TeamPageComponent} />
+            </Switch>
+            <a id='secondHalf'></a>
+          </div>
         </div>
       </Router>
     )
--- client/src/components/Teams/TeamList.jsx
+++ client/src/components/Teams/TeamList.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const TeamContainer = styled.div`
+
+`
+
+const Team = styled.div`
+margin: 0;
+h5{
+    margin 0;
+}
+`
+
 
 const TeamList = (props) => {
     return (
-        <div>
+        <TeamContainer>
             {
                 props.teams.map((team) => {
                     return (
-                        <div>
+                        <Team>
                             <h5>{team.name}</h5>
-                            {/* <Link to={`/${team.id}`}><img src={team.logo} alt={`${team.name} logo`} /></Link> */}
                             <button onClick={(event) => {
                                 event.preventDefault()
                                 props.getSingleTeam(team.id)
                             }}><img src={team.logo} alt={`${team.name} logo`} /></button>
-                        </div>
+                        </Team>
                     )
                 })
             }
-        </div>
+        </TeamContainer>
     )
 }
 
